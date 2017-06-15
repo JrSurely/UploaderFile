f6967663f23473250997aa449e3477d392b847df
JrSurely <yanzhewushuang@163.com>
2017-06-15 13:42:04 +0800

    :sparkles:大文件上传

---
+++ BigFileUploader/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigFileUploader.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #region 文件上传
+        [HttpPost]
+        public ActionResult Upload()
+        {
+            string fileName = Request["name"];
+            string fileRelName = fileName.Substring(0, fileName.LastIndexOf('.'));//设置临时存放文件夹名称
+            int index = Convert.ToInt32(Request["chunk"]);//当前分块序号
+            var guid = Request["guid"];//前端传来的GUID号
+            var dir = Server.MapPath("~/Upload");//文件上传目录
+            dir = Path.Combine(dir, fileRelName);//临时保存分块的目录
+            if (!System.IO.Directory.Exists(dir))
+                System.IO.Directory.CreateDirectory(dir);
+            string filePath = Path.Combine(dir, index.ToString());//分块文件名为索引名，更严谨一些可以加上是否存在的判断，防止多线程时并发冲突
+            var data = Request.Files["file"];//表单中取得分块文件
+            string extension = data.FileName.Substring(data.FileName.LastIndexOf(".") + 1,
+                (data.FileName.Length - data.FileName.LastIndexOf(".") - 1));//获取文件扩展名
+            //if (data != null)//为null可能是暂停的那一瞬间
+            //{
+            data.SaveAs(filePath + fileName);
+            //}
+            return Json(new { erron = 0 });//Demo，随便返回了个值，请勿参考
+        }
+        public ActionResult Merge()
+        {
+            var guid = Request["guid"];//GUID
+            var uploadDir = Server.MapPath("~/Upload");//Upload 文件夹
+            var fileName = Request["fileName"];//文件名
+            string fileRelName = fileName.Substring(0, fileName.LastIndexOf('.'));
+            var dir = Path.Combine(uploadDir, fileRelName);//临时文件夹          
+            var files = System.IO.Directory.GetFiles(dir);//获得下面的所有文件
+            var finalPath = Path.Combine(uploadDir, fileName);//最终的文件名（demo中保存的是它上传时候的文件名，实际操作肯定不能这样）
+            var fs = new FileStream(finalPath, FileMode.Create);
+            foreach (var part in files.OrderBy(x => x.Length).ThenBy(x => x))//排一下序，保证从0-N Write
+            {
+                var bytes = System.IO.File.ReadAllBytes(part);
+                fs.Write(bytes, 0, bytes.Length);
+                bytes = null;
+                System.IO.File.Delete(part);//删除分块
+            }
+            fs.Flush();
+            fs.Close();
+            System.IO.Directory.Delete(dir);//删除文件夹
+            return Json(new { error = 0 });//随便返回个值，实际中根据需要返回
+        }
+        #endregion
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
